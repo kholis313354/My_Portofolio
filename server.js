c12c2cbe831008da4c487351086939e9e372575c
kholis313354 <kholiskamaluddinw@gmail.com>
2026-05-10 23:12:47 +0700

    fix: bind express server to 0.0.0.0 for Railway

--- server.js
+++ server.js
@@ -623,6 +623,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Backend server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Backend server running on http://0.0.0.0:${PORT}`));
 
 
